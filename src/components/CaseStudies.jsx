e31b944c95caa2f7b283fe1cf9cf6feab3ef16c0
yousefkhalil894@gmail.com <yousefkhalil894@gmail.com>
2026-02-17 23:34:58 +0200

    modified:   src/components/CaseStudies.jsx

--- src/components/CaseStudies.jsx
+++ src/components/CaseStudies.jsx
@@ -1,5 +1,5 @@
 import { motion } from "motion/react";
-import Arrow from "../assets/ElLipse.svg";
+import Arrow from "/src/assets/ElLipse.svg";
 
 const studies = [
   "For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.",
